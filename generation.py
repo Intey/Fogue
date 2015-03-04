931781ffe7c6272bf8213b52acf9e060b2d5a165
Intey <ziexe0@gmail.com>
2015-03-04 23:02:33 +0000

    Add classes for map and rooms.

--- generation.py
+++ generation.py
@@ -1,3 +1,67 @@
+import random
+
+
+class Room:
+    def __init__(self, x, y, w, h):
+        self.width = w
+        self.height = h
+        self.x = x
+        self.y = y
+
+    def getConnection(self, room):
+        """Connect 2 rooms by nearest path.
+        TODO: Determine nearest sides: check horizontal check vertical.
+
+        """
+        for x in range(room.x, room.width):
+            print("in xs %s search room x:%s" % (range(self.x, self.width), x))
+            if range(self.x, self.width).__contains__(x):
+                print("overlap on x")
+                corrX = random.randint(x, self.width)
+                return corrX
+                # return corrX, max(self.y, room.y), min(self.y + self.height, room.y + room.height)
+
+        print()
+        for y in range(room.y, room.height):
+            print("in ys %s search room y:%s" % (range(self.x, self.width), y))
+            if range(self.y, self.height).__contains__(y):
+                print("overlap on y")
+                print("random position for corridor: %s" % (random.randint(y, self.height)))
+                return
+
+
+class Map:
+    def __init__(self, width, height):
+        self.tiles = [["#" for x in range(0, width)] for y in range(0, height)]
+        self.rooms = []
+
+    def mkVertCorridor(self, x, top, bottom):
+        for y in range(top, bottom):
+            self.tiles[y][x] = "."
+
+    def addRoom(self, room):
+        self.rooms.append(room)
+        for x in range(room.x, room.width):
+            for y in range(room.y, room.height):
+                self.tiles[y][x] = "."
+
+
+def printTiles(m):
+    global row, y
+    for row in m.tiles:
+        print("".join(y for y in row))
+
+
+def roomConnecting():
+    global r1, r2
+    r1 = Room(1, 1, 10, 10)
+    r2 = Room(5, 5, 10, 10)
+    r1.getConnection(r2)
+    r2 = Room(0, 0, 3, 10)
+    r1 = Room(5, 5, 10, 10)
+    r1.getConnection(r2)
+
+
 def createDungeon(roomsCnt, connections):
     rooms = createRooms(8)
     # get each pair of rooms
@@ -27,4 +91,3 @@ def createRoom(width, height):
                 if col == 0 or col == width-1:
                     room[row][col] = "#"
     return room
-
--- tests.py
+++ tests.py
@@ -7,3 +7,10 @@ def printRoom(room):
 
 room = (generation.createRoom(8, 6))
 printRoom(room)
+print()
+
+mp = generation.Map(20, 20)
+mp.addRoom(generation.Room(4, 4, 8, 8))
+mp.addRoom(generation.Room(10, 10, 15, 18))
+
+generation.printTiles(mp)
