0f38d01c8201792e9bd7492517ca66909b5934e9
alicedead <alicedead@yandex.ru>
2015-03-05 21:06:19 +0000

    new conception of rooms.

--- data.py
+++ data.py
@@ -1,24 +1,24 @@
-import io
+# import gameio
 import random
 
 
 # like interface
-class Drawable:
-    def draw(self, context):
-        pass #nothing doing there.
+#class Drawable:
+#    def draw(self, context):
+#        pass #nothing doing there. Just mean, that childs should realize this
 
 
-class Item(Drawable):
+class Item():
     def __init__(self, x, y):
         self.name="unknown"
         self.x = x
         self.y = y
-        self.color = io.BLACK
+        self.color = gameio.BLACK
     def draw(self, context):
         context.char(self.x, self.y, self.color)
 
 
-class Room(Drawable):
+class Room():
     def __init__(self, x, y, w, h):
         self.width = w
         self.height = h
@@ -55,37 +55,65 @@ class Room(Drawable):
                 return corrY, max(self.x, room.x), min(self.x + self.width, room.x + room.widht)
 
 
-class Map(Drawable):
+# split map in tiles. Each tile can contains 1+ rooms. 
+# Between tiles, we have(withount rooms borders) 1 char spacer for corridors.
+# firstly we fill tiles with rooms, then creating connections between tiles.
+# For determine which tiles should be connected, we generate random pairs 
+# of nums, that points to tiles. Nums should point to neighbors rooms.
+#     n # n # n
+#     # # # # #
+#     n # r # n
+#     # # # # #
+#     n # n # n
+# May use unique pairs list for connections - one pair will connect once.
+class Map():
     def __init__(self, width, height):
-        self.tiles = [["#" for _ in range(0, width)] for _ in range(0, height)]
+        self.points = [["#" for _ in range(0, 80)] for _ in range(0, 25)]
         self.rooms = []
         self.corridors = []
+        self.tiles = [[x for x in range(1, 8)] for y in range(1,5)]
 
 
+    def createTiles(self):
+        self.tiles = []
+        
+        
+        
     def mkVertCorridor(self, x, start_y, end_y):
         for y in range(start_y, end_y):
-            self.tiles[y][x] = "."
+            self.points[y][x] = "."
 
 
     def mkHoriCorridor(self, start_x, y, end_x):
         for x in range(start_x, end_x):
-            self.tiles[y][x] = "."
+            self.points[y][x] = "."
 
 
-    def mkCornCorridor(self, x, y, width, height):
-        self.mkVertCorridor(x, y, y+height)
-        self.mkHoriCorridor(x, y, x+width)
+    
+    def mkCornCorridor(self, x, y, width, height, corner="LU"):
+        if corner == "LU":
+            self.mkVertCorridor(x, y, y+height)
+            self.mkHoriCorridor(x, y, x+width)
+        if corner == "LB":
+            self.mkVertCorridor(x+width-1, y, y+height)
+            self.mkHoriCorridor(x, y+height-1, x+width)
+        if corner == "RU":
+            self.mkVertCorridor(x+width-1, y, y+height)
+            self.mkHoriCorridor(x, y+height-1, x+width)
+        if corner == "RB":
+            self.mkVertCorridor(x+width-1, y, y+height)
+            self.mkHoriCorridor(x, y+height-1, x+width)
 
 
     def addRoom(self, room):
         self.rooms.append(room)
-        for x in range(room.x, room.width):
-            for y in range(room.y, room.height):
-                self.tiles[y][x] = "."
+        for x in range(room.x, room.x+room.width):
+            for y in range(room.y, room.y+room.height):
+                self.points[y][x] = "."
 
 
-    def printTiles(self):
-        for row in self.tiles:
+    def printMap(self):
+        for row in self.points:
                 print("".join(y for y in row))
 
 
--- tests.py
+++ tests.py
@@ -1,9 +1,15 @@
 import data
 
+w = 8
+h = 5
+s = 5 # space
 m = data.Map(20,20)
-
-m.printTiles()
-
-m.mkCornCorridor(5, 10, 5, 5)
-
-m.printTiles()
+rc_row = 80 // (w+s)
+rc_col = 25 // (h+s) 
+print(rc_col, rc_row)
+for i in range(0, rc_col):
+    m.addRoom(data.Room(1, 1+h*i+s*i, w, h))
+    for i in range(0, rc_row):
+        m.addRoom(data.Room(1+w*i+s*i, 1, w, h))
+m.printMap()
+print()
