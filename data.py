a0be0e7a9d3becf4c224465b9765e4c3a18f5e11
Intey <ziexe0@gmail.com>
2015-03-09 04:01:53 +0000

    Many minor changes. Use tiles counts, relative sizes of all. On creation map, we give tiles count. They splits on max fit rows and cols. Rooms sizes depends on tiles sizes (not less that 2, and not more that tile size). rooms count ratio also sets in maps initialization(at this time - just magic constant 1). And many code styles by python conventions.

--- data.py
+++ data.py
@@ -1,18 +1,14 @@
 # import gameio
 import random
 import math
-# like interface
-#class Drawable:
-#    def draw(self, context):
-#        pass #nothing doing there. Just mean, that childs should realize this
 
 
 class Item():
     def __init__(self, x, y):
-        self.name="unknown"
+        self.name = "unknown"
         self.x = x
         self.y = y
-        self.color = gameio.BLACK
+
     def draw(self, context):
         context.char(self.x, self.y, self.color)
 
@@ -31,9 +27,9 @@ class Room():
             for self.y in range(self.y, self.height):
                 context.char(self.x, self.y, ".")
         for creature in self.creatures:
-            creature.draw( context )
+            creature.draw(context)
         for item in self.items:
-            item.draw( context )
+            item.draw(context)
         # return
 
     def addCreature(self, creature):
@@ -46,133 +42,119 @@ class Room():
         for x in range(room.x, room.width):
             if range(self.x, self.width).__contains__(x):
                 corrX = random.randint(x, self.width)
-                return corrX, max(self.y, room.y), min(self.y + self.height, room.y + room.height)
+                return (corrX,
+                        max(self.y, room.y),
+                        min(self.y + self.height, room.y + room.height))
 
         for y in range(room.y, room.height):
             if range(self.y, self.height).__contains__(y):
                 corrY = random.randint(y, self.height)
-                return corrY, max(self.x, room.x), min(self.x + self.width, room.x + room.widht)
+                return (corrY,
+                        max(self.x, room.x),
+                        min(self.x + self.width, room.x + room.widht))
 
-
-# split map in tiles. Each tile can contains 1+ rooms. 
-# Between tiles, we have(withount rooms borders) 1 char spacer for corridors.
-# firstly we fill tiles with rooms, then creating connections between tiles.
-# For determine which tiles should be connected, we generate random pairs 
-# of nums, that points to tiles. Nums should point to neighbors rooms.
-#     n # n # n
-#     # # # # #
-#     n # r # n
-#     # # # # #
-#     n # n # n
-# May use unique pairs list for connections - one pair will connect once.
-
-
-# Madic numbers
-
-ROOM_WIDTH  = [ 3, 5 ] # const + random
-ROOM_HEIGHT = [ 3, 5 ]
-CORRIDOR_MIN_SPACE = 3
-ROOM_RATIO = 0.8
 
 class Map():
-    def __init__(self, width, height):
+    def __init__(self, width, height, tiles_count=16):
         self.points = {}
         self.rooms = []
         self.width = width
         self.height = height
-        
-        
-        
+        self.tiles_count = tiles_count
         # no need for self, __init__ runtime only
-        tiles = self.getTiles( width, height )
-        self.fillTiles( tiles )
+        tiles = self.getTiles(tiles_count)
+        self.fillTileWithRooms(tiles, 1)  # (random.randrange(4, 11) / 10))
         # self.connectRooms()
-    
-    def getTiles(self, screenx, screeny):
-        # tile size
-        width  = ROOM_WIDTH[0]  + ROOM_WIDTH[1] 
-        height = ROOM_HEIGHT[0] + ROOM_HEIGHT[1]
-        # print( "Screen size [" + str(screenx) + "," + str(screeny) + "]" )
-        # print( "Tile size [" + str(width) + "," + str(height) + "]" )
-        # tiles count
-        # print( screenx // width )
-        
-        # there i get devide by  ))
-        # 170 % 10 == 0
-        #'couze screenx % widht == 0
-        cols = screenx // width
-        rows = screeny // height
-        
-        # print( "Tiles count [" + str(rows) + "," + str(cols) + "]" )
+
+    def getTiles(self, count):
+        """On init we know screenx, screeny - width, and height.
+        Should create count of tiles."""
+        rows, cols = getMaxDividers(count)
+
+        # assert((rows >= 2),
+        #        "%s too few rows for tiles count %s" % (rows, count))
+        # assert((cols >= 2),
+        #        "%s too few cols for tiles count %s" % (cols, count))
+
+        # tiles sizes
+        width = self.width // rows
+        height = self.height // cols
+        # TODO: needs debug screen. or log file writing
+        # print("screen w:h :: %s:%s" % (self.width, self.height))
+        # print("tiles w:h :: %s:%s" % (width, height))
+        # print("tiles rows:cols :: %s:%s" % (rows, cols))
+        # cols = self.width // width
+        # rows = self.height // height
         # prepare return
-        nums = rows * cols
+        # nums = rows * cols
         unused = []
         for r in range(0, rows):
             for c in range(0, cols):
-                unused.append(Tile( c * width, r * height, width, height ))
-        return { "nums": nums, "width": width, "height": height, "unused": unused }
-    
-    def fillTiles( self,tiles ):
-        roomsCount = math.floor( tiles[ "nums" ] * ROOM_RATIO )
-        random.shuffle( tiles[ "unused" ] )
-        for i in range( 0, roomsCount ):
-            tile = tiles[ "unused" ].pop()
-            self.createRoom( tile )
+                unused.append(Tile(c * width, r * height, width, height))
+        return unused
+
+    def fillTileWithRooms(self, tiles, room_ratio=1):
+        """Generate rooms fitted to tiles.
+        room_ratio - count of rooms. 1 - each tile."""
+        # use 11 because upper bound not included in randrange
+        rooms_count = math.floor(tiles.__len__() * room_ratio)
+        random.shuffle(tiles)
+        for i in range(0, rooms_count):
+            tile = tiles.pop()
+            self.createRoom(tile)
         return
-    
-    def fitTiles(self):
-        """ calc max tiles that can fit on map"""
-        # save it! for dragons! D @ -- gg wp
-        # -_- :%s///g - not works there. 
-        rows = ROOM_WIDTH[0] + ROOM_WIDTH[1] + CORRIDOR_MIN_SPACE
-        cols = ROOM_HEIGHT[0] + ROOM_HEIGHT[1] + CORRIDOR_MIN_SPACE
-       
-        # равновато ты их в rows пишешь
-        
-        return rows * cols
-        
+
     def createRoom(self, tile):
-        # tile instanceof class Tile
-        width  = ROOM_WIDTH[ 0 ]  + random.randint(0, ROOM_WIDTH[ 1 ] )
-        height = ROOM_HEIGHT[ 0 ] + random.randint(0, ROOM_HEIGHT[ 1 ] )
-        r_x = random.randint(tile.x, tile.x + tile.width  - width)
+        """Create room fitted to tile."""
+        width = random.randint(3, tile.width)
+        height = random.randint(3, tile.height)
+
+        assert((width <= tile.width),
+               "room width(%s) should be less or equals tile.width(%s)"
+               % (width, tile.width))
+        assert((height <= tile.height),
+               "room height(%s) should be less or equals tile.height(%s)"
+               % (height, tile.height))
+
+        r_x = random.randint(tile.x, tile.x + tile.width - width)
         r_y = random.randint(tile.y, tile.y + tile.height - height)
         for x in range(r_x, r_x + width - 1):
             for y in range(r_y, r_y + height - 1):
-                self.points[ str(x) + "_" +  str(y) ] = "."
+                self.points[str(x) + "_" + str(y)] = "."
         # return None
-        
-    def printMap(self):
-        for row in self.points:
-                print("".join(y for y in row))
 
     def draw(self, context):
-        
+        """Main draw function, for use with drawing context(IO)."""
         for key in self.points:
-            ( x, y ) = key.split( "_" )
-            char = self.points[ key ]
-            x = int(x) + 1
-            y = int(y) + 1
-            if x is 0 or y is 0:
-                print( "PIDAR!!!" )
-            context.char( char, x, y )
-        # return
-            
+            (x, y) = [int(n) for n in key.split("_")]
+            x += 1  # hack
+            y += 1  # hack
+            # assert(x != 0, "x should not be 0")
+            # assert(y != 0, "y should not be 0")
+            char = self.points[key]
+            context.char(char, x, y)
+
     def connectTiles(self):
-        pass #not ready yet
-        for i in tiles.__len__():
-            # get neighbor index. Neighbor index is in this tile index + or - 4
-            conn_idx = i
-            while (conn_idx == i): # without self
-                conn_idx = random.randint(tiles[i]-4, tiles[i]+4)
-            return conn_idx
-            
+        pass  # not ready yet
+
 
 class Tile():
     def __init__(self, x, y, w=4, h=4):
         self.x = x
         self.y = y
         self.rooms = []
-        self.width = w # minimal for room
-        self.height = h # also
-        self.unused = True
+        self.width = w   # minimal for room
+        self.height = h  # also
+
+
+def getMaxDividers(num):
+    """Return max dividers of num."""
+    devider = 1
+    rest = num
+    outdev = devider
+    while (rest > devider):
+        if num % devider == 0:
+            rest = num // devider
+            outdev = devider
+        devider += 1
+    return sorted([rest, outdev])
--- tests.py
+++ tests.py
@@ -1,21 +1,23 @@
 import gameio
 import data
-
+import random
+# for val in range(2, 64, 1):
+#    r, c = data.getMaxDividers(val)
+#    print("for num %s rows %s and cols %s" % (val, r, c))
 out = gameio.Output()
 inp = gameio.Input()
-
 out.page()
-
-( w, h ) = out.size()
+(w, h) = out.size()
+tiles_counts = [6, 9, 12, 16, 32]
 while 1:
     out.clear()
-    map = data.Map( w, h ) # auto generate rooms and corrs on __init___
-    map.draw( out )
+    map = data.Map(w, h, random.choice(tiles_counts))  # auto generate rooms and corrs on __init___
+    map.draw(out)
     ch = inp.char()
     if ch is "escape":
         out.reset()
         break
-    
+
 # out.move( 0, 15 )
 
 # w = 8
@@ -23,7 +25,7 @@ while 1:
 # s = 3 # space
 # m = data.Map(20,20)
 # rc_row = 80 // (w+s)
-# rc_col = 25 // (h+s) 
+# rc_col = 25 // (h+s)
 # print(rc_col, rc_row)
 # for i in range(0, rc_col):
 #     m.addRoom(data.Room(3, 2+h*i+s*i, w, h))
